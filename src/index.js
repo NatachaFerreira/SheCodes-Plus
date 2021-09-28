32bc14e5288280543a671a543deb8100ccfd8c1c
NatachaFerreira <natacha_ferreira_@hotmail.com>
2021-09-28 10:47:12 +0000

    Updated weekly forecast

--- src/index.js
+++ src/index.js
@@ -16,6 +16,21 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp *1000);
+  let day = date.getDay();
+  let days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+  ];
+  return days[day];
+}
+
 function handleSearch(event) {
   event.preventDefault();
   getLocationBySearch(document.querySelector("#city-input").value);
@@ -133,24 +148,27 @@ function displayFarenheitTemperature(event) {
 }
 
 function displayWeeklyForecast(response) {
-  console.log(response.data);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  
   let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay, index) {
+    if(index < 6) {
     forecastHTML =
       forecastHTML +
       `
           <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
-            <img src="images/sunny.png" alt="sunny" width="70px"/>
+            <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+            <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="${forecastDay.weather[0].description}" width="90px"/>
             <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-max-temperature">25°</span>
-              <span class="weather-forecast-min-temperature">21°</span>
+              <span class="weather-forecast-max-temperature">${Math.round(forecastDay.temp.max)}°</span>
+              <span class="weather-forecast-min-temperature">${Math.round(forecastDay.temp.min)}°</span>
             </div>
           </div>
       `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
